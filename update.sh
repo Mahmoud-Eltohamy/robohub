fabeb9ad66b976fdae42306348a1d64a1a0be6ec
Mahmoud-Eltohamy <mahmoud.mohammed.elhady@gmail.com>
2020-04-03 00:04:58 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,2 +0,0 @@
-# After JupyterHub has started up, run this command to update it settings according to the config.yaml file
-helm upgrade jhub jupyterhub/jupyterhub --version=0.8-ccc1e6b --values config.yaml
